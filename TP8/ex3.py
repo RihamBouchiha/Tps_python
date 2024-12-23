9ac1b8aea30214ba62fad92357cb33234f9ca346
riham bouchiha <riham.bouchiha@usmba.ac.ma>
2024-12-23 10:46:32 +0100

    cmt2

--- TP8/ex3.py
+++ TP8/ex3.py
@@ -1,6 +1,19 @@
 class myString:
-    def __init__(self,chaine):
-        self.chaine = chaine
+    def __init__(self, str):
+        self.str = str
 
-    def append(self,chaine):
-        
+    def append(self, added_str):
+        return self.str + added_str
+
+    def pop(self, index):
+        str = ""
+        for i, value in enumerate(self.str):
+            if i == index:
+                continue
+            str += value
+        print(str)
+
+
+s1 = myString("Hello")
+print(s1.append(" world!"))
+s1.pop(2)
